fad39e94f205e11f0afbb6762052361aa312cfa4
amaanlari <amaanlari@outlook.com>
2023-09-27 05:20:12 +0530

    Created the layout infrastructure

--- app/src/main/java/com/lari/diceroller/MainActivity.kt
+++ app/src/main/java/com/lari/diceroller/MainActivity.kt
@@ -3,7 +3,11 @@ package com.lari.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lari.diceroller.ui.theme.DiceRollerTheme
 
@@ -21,10 +25,13 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage()
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
 }
 
 @Composable
-fun DiceWithButtonAndImage() {
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 
 }
